90351578d6e465d4cb65f8ad4649aa15bebe7ca9
Your Name <you@example.com>
2017-10-05 16:50:28 +0530

    initial controller commit

---
+++ HotelReservation/HotelReservationEngine/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace HotelReservationEngine.Controllers
+{
+    [Route("/")]
+    public class ValuesController : Controller
+    {
+        [HttpGet()]
+        [HttpGet("/api/values")]
+        [HttpGet("index")]
+        public IActionResult GetIndexPage()
+        {
+            return File(new FileStream("wwwroot/HomePage/index.html", FileMode.Open), "text/html");
+        }
+    }
+}
