01553789c45e8ab6d4c5916b83e6d85be6f6711b
Pritesh Dhokchaule <pdhokchaule@tavisca.com>
2017-10-13 01:04:06 +0530

    Addign fix

--- HotelReservation/HotelReservationEngine/wwwroot/Js/roomListingScript.js
+++ HotelReservation/HotelReservationEngine/wwwroot/Js/roomListingScript.js
@@ -5,24 +5,28 @@ $(document).ready(function () {
     roomList = sessionStorage.getItem("rooms");
     room = JSON.parse(roomList);
 
-    var roomType = [];
-    var img="";
+    var roomTypes = [];
+    var img = "";
     for (var i = 0; i < room.itinerary.rooms.length; i++) {
         img = room.itinerary.hotelProperty.mediaContent[i].url.toString();
-        roomType.push({
+        roomTypes.push({
 
-            hotelname: room.itinerary.hotelProperty.name,
             description: room.itinerary.rooms[i].roomDescription,
             address: room.itinerary.hotelProperty.address.completeAddress,
             roomtype: room.itinerary.rooms[i].roomName,
             price: room.itinerary.rooms[i].displayRoomRate.totalFare.amount,
-            imageurl: img
         });
 
     }
+    var hotel = {
+        name: room.itinerary.hotelProperty.name,
+        address: room.itinerary.hotelProperty.address.completeAddress,
+        imageurl: img,
+        roomTypes: roomTypes
+    };
     var template = $('#room-item');
     var compiledTemplate = Handlebars.compile(template.html());
-    var html = compiledTemplate(roomType);
+    var html = compiledTemplate(hotel);
     $('#roomList-container').html(html);
 }
 );
@@ -30,11 +34,9 @@ var roomName
 function roomPrice(data1) {
     console.log(data1);
     roomName = data1.value;
-    for (i = 0; i < room.itinerary.rooms.length; i++)
-    {
+    for (i = 0; i < room.itinerary.rooms.length; i++) {
         var check = room.itinerary.rooms[i].roomName.toString();
-        if (roomName.toString() == check)
-        {
+        if (roomName.toString() == check) {
             var data1 =
                 {
                     "Itinerary": room.itinerary,
