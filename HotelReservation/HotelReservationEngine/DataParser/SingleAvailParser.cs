e805f1d6c755a0937417417e7fd10dfd30a7ffa8
Your Name <you@example.com>
2017-10-26 17:18:07 +0530

    refactored v2

--- HotelReservation/HotelReservationEngine/DataParser/SingleAvailParser.cs
+++ HotelReservation/HotelReservationEngine/DataParser/SingleAvailParser.cs
@@ -51,34 +51,6 @@ namespace HotelReservationEngine.DataParser
                 Criteria= singleAvailItinerary.Criteria
             };
             var cache=Cache.AddToCache(singleAvail);
-            //List<RoomInfo> roomInfo = new List<RoomInfo>();
-            //for (int i = 0;i < hotelRoomAvailRS.Itinerary.Rooms.Length;i++)
-            //{
-            //    string imageUrl = "";
-            //    for (int j = 0; j <= hotelRoomAvailRS.Itinerary.HotelProperty.MediaContent.Length; j++)
-            //    {
-            //        if (hotelRoomAvailRS.Itinerary.HotelProperty.MediaContent[j].Url != null)
-            //        {
-            //            imageUrl = hotelRoomAvailRS.Itinerary.HotelProperty.MediaContent[j].Url.ToString();
-            //            break;
-            //        }
-            //    }
-            //    RoomInfo room = new RoomInfo()
-            //    {
-            //        GuidId=cache,
-            //        HotelName= hotelRoomAvailRS.Itinerary.HotelProperty.Name,
-            //        ImageUrl=imageUrl,
-            //        price= hotelRoomAvailRS.Itinerary.Rooms[i].DisplayRoomRate.BaseFare.Amount,
-            //        RoomDiscription= hotelRoomAvailRS.Itinerary.Rooms[i].RoomDescription,
-            //        RoomName= hotelRoomAvailRS.Itinerary.Rooms[i].RoomName,
-            //        Address=hotelRoomAvailRS.Itinerary.HotelProperty.Address.CompleteAddress,
-            //        Rating=hotelRoomAvailRS.Itinerary.HotelProperty.HotelRating.Rating,
-            //        Distance= hotelRoomAvailRS.Itinerary.HotelProperty.Distance.Amount,
-            //        Duration= hotelRoomAvailRS.Itinerary.StayPeriod.Duration
-            //    };
-            //    roomInfo.Add(room);
-            //}
-            //var result = new RoomList() { Rooms = roomInfo };
             return singleAvail;
         }
     }
