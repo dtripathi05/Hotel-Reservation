04b3696d52d5baf8712ee9574875fb452f12cd27
Amit Prakash <aprakash@internal.tavisca.com>
2017-10-30 10:50:50 +0530

    change in regex of credit card validation

--- HotelReservation/HotelReservationEngine/wwwroot/Js/guestDetailsScript.js
+++ HotelReservation/HotelReservationEngine/wwwroot/Js/guestDetailsScript.js
@@ -108,7 +108,7 @@ function importDetails() {
 
 function validateCardNumber(cardNumber) {
 
-    if (/^[0-9]{13,14,15,16}$/.test(cardNumber)) {
+    if (/^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\d{3})\d{11})$/.test(cardNumber)) {
         if (luhnCheck(cardNumber)) {
             flag = true;
         }
